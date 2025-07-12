6dca102cc068da90b8c55103cabee4dcedff7cad
Gaurav-Ai-Engineer <anuvidyainfotech10@gmail.com>
2025-07-12 12:22:00 +0530

    image added

--- one.py
+++ one.py
@@ -1,2 +1,37 @@
 import streamlit as st
 st.title('Git Learning')
+st.header('Our courses')
+st.subheader('python')
+st.subheader('sql')
+st.subheader('c')
+st.subheader('c++')
+st.subheader('java')
+col1,col2=st.columns(2)
+with col1:
+    st.image('D:\pr1\image.png')
+with col2:
+    st.write('''
+The name "Fantasia flower" likely refers to the Hibiscus 'Fantasia', a cultivar known for its large, ruffled, rosy pink flowers with a dark red center. It is a compact, sturdy hibiscus that typically grows to 2-3 feet tall and produces 8-9 inch wide flowers. There is also a rose called 'Fantasia Mondiale Freelander' with salmon-orange blooms. Additionally, "Fantasia" is a term used in the context of floral designs, like those found at Floral Fantasia and in embroidery designs. 
+Oops, something went wrong.
+Floral Fantasia
+We couldn't be happier. Thank you Murjhana and staff!" -Melinda Altshuler. "Did an amazing job on my wedding - better than I ever ...
+Floral Fantasia
+
+Flower fantasia hi-res stock photography and images - Alamy
+RM KJWWRT–This rose with the name 'Fantasia Mondiale Freelander' was discovered by Karsten Schmidt and introduced to the German ma...
+
+Alamy
+
+Exclusive Fantasia Flower Hand Embroidery Design
+18 Jan 2025 — Exclusive Fantasia Flower Hand Embroidery Design. DIY And Crafts. Fabric Crafts. More about this Pin. Board containing ...
+
+
+Pinterest · 
+amumtahena
+
+Show all
+
+
+
+
+''')
